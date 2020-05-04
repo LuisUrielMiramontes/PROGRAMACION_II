c9836d6f4463c4c490299e6b1d6c32a153539ccd
LuisUrielMiramontes <icebreakermatrix@gmail.com>
2020-05-04 09:49:27 -0600

    Oasis

---
+++ Unidad5/Torneo/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Torneo
+{
+    public partial class Form1 : Form
+    {
+        /*Aquí se crea el objeto para mandar a llamar las propiedades y métodos creadas en la clase
+         "Torneo"*/
+        Torneo objTorneo = new Torneo();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void lblEmpate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            /*Esto solamente cierra el programa*/
+            Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            /*Esto guarda los datos ingresados del programa que despues de ingresarlos, manda un
+             mensaje indicando que ya los guardó*/
+            objTorneo.NombreTorneo = Convert.ToString(txtNombreTorneo.Text);
+            objTorneo.NumEquipos = (int)nudNumEquipos.Value;
+            objTorneo.FechaInicio = dtpFechaInicio.Value;
+            objTorneo.FechaFin = dtpFechaFin.Value;
+            MessageBox.Show("Los datos se han guardado correctamente.");
+        }
+
+        private void btnResultados_Click(object sender, EventArgs e)
+        {
+            /*Aquí se usa el objeto para poder mandar a llamar las propiedades y métodos para
+             darles un valor y así poder usarlos por medio de los arreglos con el fin de sacar
+             el resultado del partido*/
+            objTorneo.PuntajeXPartidos = new string[objTorneo.NumEquipos,objTorneo.NumEquipos];
+            for (int f = 0; f < objTorneo.PuntajeXPartidos.GetLength(0); f++)
+            {
+                objTorneo.PuntajeXPartidos[f, 0] = Interaction.InputBox("Introduce el nombre del equipo #" + (f + 1));
+                for (int c = 1; c < objTorneo.PuntajeXPartidos.GetLength(1); c++)
+                {
+                    objTorneo.PuntajeXPartidos[f, c] = Interaction.InputBox("Introduce el puntaje obtenido en el partido #" + c );                    
+                }
+                MessageBox.Show("Los resultados han sido ingresados correctamente.");
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            /*Esto solamente limpia los datos ingresados para poder ingresar datos nuevos*/
+            txtNombreTorneo.Text = "";
+            nudNumEquipos.ResetText();
+            dtpFechaInicio.ResetText();
+            dtpFechaFin.ResetText();
+        }
+
+        private void btnGanador_Click(object sender, EventArgs e)
+        {
+            /*Aquí se imprime los datos finales del ganador del torneo*/
+            objTorneo.PuntosFinal = new int[objTorneo.NumEquipos];
+            objTorneo.CalcularPuntosObtenidos();
+            MessageBox.Show(objTorneo.CalcularGanador());
+        }
+    }
+}
---
+++ Unidad5/Torneo/Torneo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torneo
+{
+    class Torneo
+    {
+        //Estas propiedes se crean aquí para despues heredarlas al "Forms1.cs"
+        public string NombreTorneo { get; set; }
+        public int NumEquipos { get; set; }
+        public int NumPartidos { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public int PuntosGanador { get; set; }
+        public int PuntosEmpate { get; set; }
+        public int PuntosPerdedor { get; set; }
+        public int[] PuntosFinal { get; set; }
+        public string[,] PuntajeXPartidos { get; set; }
+        /*Como su mismo nombre lo dice, este método sirve para calcular los puntos obtenidos
+         de un partido*/
+        public void CalcularPuntosObtenidos()
+        {
+            int p = 0;
+            for (int i = 0; i < PuntajeXPartidos.GetLength(0); i++)
+            {
+                int suma=0;
+                int acum = 0;
+                for (int j = 1; j < PuntajeXPartidos.GetLength(1); j++)
+                {
+                    suma = Convert.ToInt32(PuntajeXPartidos[i, j]);
+                    acum += suma;
+                }
+                PuntosFinal[p] = acum;
+                p++;
+            }
+        }
+        /*Mientras que este método, sirve para calcular el ganador por medio de un ciclo "for"*/
+        public string CalcularGanador()
+        {
+            int NumGanador = 0;
+            int f = 0;
+            int NumMayor = 0;
+            string Ganador = "";
+            for (int i = 0; i < PuntosFinal.Length; i++)
+            {
+                if (PuntosFinal[i] > NumMayor)
+                {
+                    NumMayor = PuntosFinal[i];
+                    NumGanador = f;
+                }
+                f++;
+            }
+            Ganador = "¡El equipo ganador del torneo " + NombreTorneo + " es: " + PuntajeXPartidos[NumGanador, 0] + " con " + NumMayor + " puntos! ¡Felicidades!";
+            return Ganador;
+        }
+    }
+}
