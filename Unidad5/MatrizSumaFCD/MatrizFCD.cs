5a59d81188ae23e8e18c6f2f8291c13f802ec143
LuisUrielMiramontes <icebreakermatrix@gmail.com>
2020-05-08 00:22:59 -0600

    Wryyy

---
+++ Unidad5/MatrizSumaFCD/MatrizFCD.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizSumaFCD
+{
+    class MatrizFCD
+    {
+        //Estas propiedes se crean aquí para despues heredarlas al "Forms1.cs"
+        public int [,] arregloMatriz { set; get; }
+        public int filas { set; get; }
+        public int[] acumFilas { set; get; }
+        public int columnas { set; get; }
+        public int[] acumColumnas { set; get; }
+        public int diagonal { set; get; }
+        public int[] acumDiagonal { set; get; }
+        /*Este método sirve para poder sumar los datos acumulados de la fila de la matriz*/
+        public void SumarFilas()
+        {
+            int f = 0;
+            for (int i = 0; i < arregloMatriz.GetLength(0); i++)
+            {
+                int suma = 0;
+                int acum = 0;
+                for (int j = 1; j < arregloMatriz.GetLength(1); j++)
+                {
+                    suma = Convert.ToInt32(arregloMatriz[i, j]);
+                    acum += suma;
+                }
+                acumFilas[f] = acum;
+                f++;
+            }
+        }
+        /*Este método sirve para poder sumar los datos acumulados de la columna de la matriz*/
+        public void SumarColumnas()
+        {
+            int c = 0;
+            for (int i = 0; i < arregloMatriz.GetLength(0); i++)
+            {
+                int suma = 0;
+                int acum = 0;
+                for (int j = 1; j < arregloMatriz.GetLength(1); j++)
+                {
+                    suma = Convert.ToInt32(arregloMatriz[i, j]);
+                    acum += suma;
+                }
+                acumColumnas[c] = acum;
+                c++;
+            }
+        }
+        /*Este método sirve para poder sumar los datos acumulados acomodados en forma diagonal
+         *de la matriz*/
+        public void SumarDiagonal()
+        {
+            int d = 0;
+            for (int i = 0; i < arregloMatriz.GetLength(0); i++)
+            {
+                int suma = 0;
+                int acum = 0;
+                for (int j = 1; j < arregloMatriz.GetLength(1); j++)
+                {
+                    suma = Convert.ToInt32(arregloMatriz[i, j]);
+                    acum += suma;
+                }
+                acumDiagonal[d] = acum;
+                d++;
+            }
+        }
+    }
+}
---
+++ Unidad5/NumerosMayorMenor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosMayorMenor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeros objNumeros = new Numeros();
+            Console.WriteLine("¿Cuántos números deseas ingresar?");
+            objNumeros.ingresarNumeros = int.Parse(Console.ReadLine());
+            for (int i = 0; i < objNumeros.ingresarNumeros; i++)
+            {
+                Console.WriteLine("Ingrese el número #" + (i + 1) + ":");
+                objNumeros.ingresarNumeros = int.Parse(Console.ReadLine());
+            }
+            {
+                if (objNumeros.menor < objNumeros.mayor)
+                {
+                    Console.WriteLine("El número mayor es: " + objNumeros.mayor);
+                    Console.WriteLine("El número menor es: " + objNumeros.menor);
+                }
+                Console.WriteLine(objNumeros.SacarMayorMenor());
+                Console.ReadLine();
+            }
+        }
+    }
+}
